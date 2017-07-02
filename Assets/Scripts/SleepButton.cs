768de8e19aa2692ff1a9a71f92743b0ba6c6349b
ialy1595 <ialy12006@gmail.com>
2017-07-02 20:24:53 +0900

    button example

---
+++ Assets/Scripts/SleepButton.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SleepButton : MonoBehaviour
+{
+
+    private Text sleepText;
+
+    // Use this for initialization
+    void Start()
+    {
+        sleepText = GetComponentInChildren<Text>();
+        if (GameManager.gm.isSleeping) sleepText.text = "Awake";
+        else sleepText.text = "Sleep";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.gm.isSleeping) sleepText.text = "Awake";
+        else sleepText.text = "Sleep";
+    }
+
+    public void OnClick()
+    {
+        if (GameManager.gm.isPaused) return;
+        if(!GameManager.gm.isSleeping) GameManager.gm.sleepStartTime = GameManager.gm.getPlayTime();
+        GameManager.gm.isSleeping = !GameManager.gm.isSleeping;
+    }
+}
---
+++ Assets/Scripts/StaminaText.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StaminaText : MonoBehaviour
+{
+
+    private Text Timetext;
+
+    // Use this for initialization
+    void Start()
+    {
+        Timetext = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int intStamina = (int)(GameManager.gm.status.getStatus((int)Status.StatusType.Stamina));
+        Timetext.text = "" + intStamina;
+    }
+}
