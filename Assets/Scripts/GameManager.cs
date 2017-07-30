f052143a59153f2e0f1e471f708e866fffbb5e83
gkdjrn <guswo1541>
2017-07-30 22:51:02 +0900

    dog

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -39,7 +39,19 @@ public class GameManager : MonoBehaviour
 
     //part time job
     [HideInInspector] public float jobStartTime;
-    
+
+    //dog
+    [HideInInspector] public bool dogPlaying;
+    public void dogStart()
+    {
+        dogPlaying = true;
+    }
+
+    public void dogEnd()
+    {
+        dogPlaying = false;
+    }
+
 
     //scene
     public GameObject mainScene;
---
+++ Assets/Scripts/dogscript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dogscript : MonoBehaviour
+{
+    private float dog_playTIme;
+    public GameObject dog;
+    public GameObject sleeping_dog;
+
+    // Use this for initialization
+    void Start()
+    {
+        dog = transform.Find("dog").gameObject;
+        sleeping_dog = transform.Find("sleeping_dog").gameObject;
+        dog.SetActive(true);
+        sleeping_dog.SetActive(false);
+        GameManager.gm.dogPlaying = false;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.gm.isPaused)
+            return;
+        if (GameManager.gm.dogPlaying && (GameManager.gm.getPlayTime() - dog_playTIme > 30f))
+            GameManager.gm.dogEnd();
+
+    }
+
+    void OnClick()
+    {
+        if (GameManager.gm.isPaused) return;
+
+        dog_playTIme = GameManager.gm.getPlayTime();
+        dog.SetActive(false);
+        sleeping_dog.SetActive(true);
+        GameManager.gm.dogStart();
+    }
+}
