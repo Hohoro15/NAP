fc54058912887d62f1ad646ccb448d55f0ab995f
ialy1595 <ialy12006@gmail.com>
2017-07-31 15:22:37 +0900

    save and load

---
+++ Assets/saveAndLoad.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class saveAndLoad : MonoBehaviour {
+
+    int number;
+    Text text;
+
+	// Use this for initialization
+	void Start ()
+    {
+        number = PlayerPrefs.GetInt("Number",0);
+        text = transform.Find("saveText").GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        text.text = "" + number;
+	}
+
+    public void onClickInc()
+    {
+        number++;
+    }
+
+    public void onClickSave()
+    {
+        PlayerPrefs.SetInt("Number",number);
+    }
+}
